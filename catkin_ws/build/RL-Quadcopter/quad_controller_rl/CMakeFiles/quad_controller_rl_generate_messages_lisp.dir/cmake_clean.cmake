b6a1e5719cacd0c34fbc64eaa342a22bc48821cb
Jonas Chan <jonas.chan@holovis.com>
2018-02-19 18:56:29 +0000

    Temp removal

--- catkin_ws/build/RL-Quadcopter/quad_controller_rl/CMakeFiles/quad_controller_rl_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/quad_controller_rl_generate_messages_lisp"
-  "/home/robond/Desktop/catkin_ws/devel/share/common-lisp/ros/quad_controller_rl/srv/SetPose.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/quad_controller_rl_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- catkin_ws/build/RL-Quadcopter/quad_controller_rl/catkin_generated/quad_controller_rl-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(quad_controller_rl_MESSAGE_FILES "")
-set(quad_controller_rl_SERVICE_FILES "/home/robond/Desktop/catkin_ws/src/RL-Quadcopter/quad_controller_rl/srv/SetPose.srv")
