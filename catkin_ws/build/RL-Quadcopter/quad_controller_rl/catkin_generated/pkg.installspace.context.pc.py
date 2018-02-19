bef4c0f4db3b334e226c48b1f8b6eb83029416d3
robond <jonas.chan@holovis.com>
2018-02-19 10:32:42 -0800

    "Initial commit"

---
+++ catkin_ws/build/RL-Quadcopter/quad_controller_rl/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/robond/Desktop/catkin_ws/install/include".split(';') if "/home/robond/Desktop/catkin_ws/install/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime;std_msgs;geometry_msgs;std_srvs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "quad_controller_rl"
+PROJECT_SPACE_DIR = "/home/robond/Desktop/catkin_ws/install"
+PROJECT_VERSION = "0.0.0"
